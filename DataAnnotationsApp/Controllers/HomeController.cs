e941661cbbd1df5631dd6b180f5cc43cf5e74741
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-12-19 23:48:42 +0300

    lesson 44

--- DataAnnotationsApp/Controllers/HomeController.cs
+++ DataAnnotationsApp/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataAnnotationsApp.Models;
 
 namespace DataAnnotationsApp.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Edit()
+        {
+            LogModel logModel = new LogModel { Login = "asdasda", Password = "asdaf" };
+            return View(logModel);
+        }
+
+        public ActionResult Details()
+        {
+            var book = db.Books.FirstOrDefault();
+            return View(book);
         }
 
         public ActionResult About()
---
+++ DataAnnotationsApp/Models/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace DataAnnotationsApp.Models
+{
+    public class BookContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+
+    //public class BookDbIntializer : DropCreateDatabaseAlways<BookContext>
+    //{
+    //    protected override void Seed(BookContext db)
+    //    {
+    //        db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Year = 1803 });
+    //        db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Year = 1899 });
+    //        db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Year = 1843 });
+    //        base.Seed(db);
+    //    }
+    //}
+}
